32fe5d3ca180f8ae5e88edae42d8de3cab3d2486
MoeAlshidi <Moealshidi@gmail.com>
2022-09-21 18:51:39 +0400

    created main models:

--- fitapp/models.py
+++ fitapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Measurement(models.Model):
+    
+    pass
+
+
+
+class Media(models.Model):
+    TYPE_FRONT='F'
+    TYPE_BACK='B'
+    TYPE_SIDE='S'
+    TYPE_CHOICES=[
+        (TYPE_FRONT,'Front'),
+        (TYPE_BACK,'Back'),
+        (TYPE_SIDE,'Side'),
+    ]
+
+
+class Customer(models.Model):
+    user=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    age=models.PositiveIntegerField()
+    measurement=models.ForeignKey(Measurement,on_delete=models.CASCADE,related_name='measurements')
+
+
+
