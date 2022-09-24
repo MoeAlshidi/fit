c11e6ef89b59111dcec18ef5b409373793b924cd
MoeAlshidi <Moealshidi@gmail.com>
2022-09-24 14:29:01 +0400

    Auth, jwt token, created related user id

--- fitapp/serializers.py
+++ fitapp/serializers.py
@@ -1,12 +1,11 @@
 from rest_framework import serializers
 from .models import Customer,Measurement
 
-
 class MeasurementsSerializer(serializers.ModelSerializer):
-
+    customer_id=serializers.PrimaryKeyRelatedField(read_only=True)
     class Meta:
         model=Measurement
-        fields=['id','date','weight','bust','calves','chest','waist','customer']
+        fields=['id','date','weight','bust','calves','chest','waist','customer_id']
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model=Customer
--- fitapp/views.py
+++ fitapp/views.py
@@ -1,14 +1,13 @@
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.viewsets import GenericViewSet,ModelViewSet
-from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin,UpdateModelMixin,ListModelMixin
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin,UpdateModelMixin
+from rest_framework.permissions import IsAuthenticated
 from .models import Customer, Measurement
 from .serializers import CustomerSerializer,MeasurementsSerializer
 
 
 
-
-# Create your views here.
 class CustomerViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin,GenericViewSet):
     queryset=Customer.objects.all()
     serializer_class=CustomerSerializer
@@ -16,8 +15,12 @@ class CustomerViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin,Gen
 
 
 class MeasurementViewSet(ModelViewSet):
-    http_method_names=['get','post', 'put']
+    http_method_names=['get','put','post']
     serializer_class=MeasurementsSerializer
+    permission_classes=[IsAuthenticated]
     queryset=Measurement.objects.all()
     
-
+    def perform_create(self, serializer):
+        serializer.save(customer_id=self.request.user.id)
+    def get_queryset(self):
+        return Measurement.objects.filter(customer_id=self.request.user.id)
